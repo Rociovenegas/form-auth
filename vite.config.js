cd614e10a1242d412209e9c5cc8489ce24a6655c
Rocio <rociovenegasrojas.06@gmail.com>
2026-07-03 17:31:08 -0400

    fix name

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/duolingo-auth/", // debe coincidir con el nombre de tu repo en GitHub
+  base: "/form-auth/", // debe coincidir con el nombre de tu repo en GitHub
 });
